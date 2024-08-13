1a4ef82503e6053bec351238e21bf46205ccfc86
Sanket12122003 <sanketpawarsp1212@gmail.com>
2024-08-13 08:48:25 +0530

    code commited

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'QApplication',
+    version= '0.0.1',
+    author= 'sanket pawar',
+    author_email= 'sanket.pawar@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
